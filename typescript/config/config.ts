ae5c88459dbce6db67f67c5e9afe47d030716710
dakom <david.komer@gmail.com>
2019-10-31 00:05:48 +0200

    debug setting

--- typescript/config/config.ts
+++ typescript/config/config.ts
@@ -4,20 +4,17 @@ export const isProduction = buildMode === "production" ? true : false;
 
 interface DebugSettings {
     skipStart: boolean;
-    muteAudio: boolean;
     maxFractalIterations: number;
 }
 
 const devDebugSettings:DebugSettings = {
-    skipStart: true,
-    //skipStart: false,
-    muteAudio: true,
+    //skipStart: true,
+    skipStart: false,
     maxFractalIterations: 25
 }
 
 const prodDebugSettings:DebugSettings = {
     skipStart: false,
-    muteAudio: false,
     maxFractalIterations: 25 
 }
 
